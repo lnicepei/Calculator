48b9f73ab9984a896cb6048dde722e82592301bb
lnicepei <dmitrii.shaidorov@gmail.com>
2022-03-10 21:38:22 +0300

    Back up to save calculator 1.0

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let operation = "",
   operationCounter = 0,
   index = 0,
   newString = "",
-  operationsArray =[],
-  operationIndex = 0;//counts number of elements in operationsArray
+  operationsArray = [],
+  operationIndex = 0; //counts number of elements in operationsArray
 
 let btns = document.getElementsByClassName("buttonDigit");
 let btnsArray = [...btns];
@@ -17,30 +17,33 @@ document.getElementById("clear").addEventListener("click", clarifier);
 document.getElementById("delete").addEventListener("click", deleter);
 document.getElementById("dot").addEventListener("click", dotter);
 
-window.addEventListener('keydown', function(e){
-  if(e.key >= 0 && e.key <= 9){
+window.addEventListener("keydown", function (e) {
+  if (e.key >= 0 && e.key <= 9) {
     digitButtonAssigner(e.key);
     buttonColorStart(e);
   }
-  if(e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/" || e.key == "=") operate(e.key);
-  if(e.key == '.') dotter();
-  if(e.key == "Backspace") deleter();
-  if(e.key == "Delete") clarifier();
+  if (e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/")
+    operate(e.key);
+  if (e.key == "=") equalizer();
+  if (e.key == ".") dotter();
+  if (e.key == "Backspace") deleter();
+  if (e.key == "Delete") clarifier();
 });
 
-const buttons = document.querySelectorAll('.buttonDigit');
-buttons.forEach(button => button.addEventListener('transitionend', buttonColorEnd));
+const buttons = document.querySelectorAll(".buttonDigit");
+buttons.forEach((button) =>
+  button.addEventListener("transitionend", buttonColorEnd)
+);
 
-function buttonColorEnd(e){
-  if(e.propertyName !== 'transform') return;
-  this.classList.remove('tapping');
-  console.log(e.propertyName)
+function buttonColorEnd(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("tapping");
 }
 
-function buttonColorStart(e){
+function buttonColorStart(e) {
   const key = document.querySelector(`.buttonDigit[data-key ="${e.keyCode}"]`);
   console.log(e.keyCode);
-  key.classList.add('tapping');
+  key.classList.add("tapping");
 }
 
 main();
@@ -63,12 +66,16 @@ function digitButtonAssigner(btn) {
     document.getElementById("screen").textContent = ""; //if() checks if there are digits on the screen after operations
   }
   if (document.getElementById("screen").textContent == "0") {
-    document.getElementById("screen").textContent = "";         //forbids to add many 0s 
+    document.getElementById("screen").textContent = ""; //forbids to add many 0s
   }
-  if (parseFloat(document.getElementById("screen").textContent).toString().length < 11) {
-    if(btn.srcElement !== undefined){
-      document.getElementById("screen").textContent += btn.srcElement.textContent;
-    }else if(btn <= 9 && btn >= 0 || btn == "."){
+  if (
+    parseFloat(document.getElementById("screen").textContent).toString()
+      .length < 11
+  ) {
+    if (btn.srcElement !== undefined) {
+      document.getElementById("screen").textContent +=
+        btn.srcElement.textContent;
+    } else if ((btn <= 9 && btn >= 0) || btn == ".") {
       document.getElementById("screen").textContent += btn;
       // btn.classList.add('button:hover')
       console.log(btn);
@@ -78,21 +85,39 @@ function digitButtonAssigner(btn) {
 }
 function operationChoice(btn) {
   if (index != 0) {
-    // if(operationIndex > 4){
-    //   let temp = operationsArray[operationIndex - 1];
-    //   operationIndex = 0;
-    //   operationsArray = [];
-    //   operationsArray.push(temp);
-    // }
-    if(btn == "+" || btn == "-" || btn == "*" || btn == "/" || btn == "="){
+    if (operationIndex > 4) {
+      let temp = operationsArray[operationIndex - 1];
+      console.log(operationsArray[operationIndex - 1]);
+      operationIndex = 1;
+      operationsArray = [];
+      operationsArray.push(temp);
+    }
+    if (btn == "=" || btn.srcElement.textContent == "=") {
+      document.getElementById("screen").textContent +=
+        operationsArray[operationIndex - 1];
+      return btn.srcElement.textContent;
+    }
+    if (btn == "+" || btn == "-" || btn == "*" || btn == "/" || btn == "=") {
       operationsArray.push(btn);
+      document.getElementById("screen").textContent +=
+        operationsArray[operationIndex - 1];
       console.log(operationsArray);
-    }else if(btn.srcElement.textContent == "+" || btn.srcElement.textContent == "-" || btn.srcElement.textContent == "*" || btn.srcElement.textContent == "/" || btn.srcElement.textContent == "="){
+    } else if (
+      btn.srcElement.textContent == "+" ||
+      btn.srcElement.textContent == "-" ||
+      btn.srcElement.textContent == "*" ||
+      btn.srcElement.textContent == "/" ||
+      btn.srcElement.textContent == "="
+    ) {
       operationsArray.push(btn.srcElement.textContent);
       console.log(operationsArray);
+      document.getElementById("upperScreen").textContent +=
+        document.getElementById("screen").textContent;
       console.log(operationsArray[operationIndex - 1]);
     }
-    
+    document
+      .getElementById("screen")
+      .appendChild(operationsArray[operationIndex - 1]);
     return operationsArray[operationIndex - 1];
   }
   index++;
@@ -111,30 +136,35 @@ function operate(btn) {
     if (operation === "+") {
       result = currentNumber + previousNumber;
       previousNumber = result;
-      screenUpdater(result);
+      // screenUpdater(result);
       // document.getElementById("screen").textContent = result;
     } else if (operation === "-") {
       result = -currentNumber + previousNumber;
       previousNumber = result;
-      screenUpdater(result);
+      // screenUpdater(result);
       // document.getElementById("screen").textContent = result;
     } else if (operation === "*") {
       result = currentNumber * previousNumber;
       previousNumber = result;
-      screenUpdater(result);
+      // screenUpdater(result);
       // document.getElementById("screen").textContent = result;
     } else if (operation === "/") {
       result = previousNumber / currentNumber;
       previousNumber = result;
-      screenUpdater(result);
+      // screenUpdater(result);
     } else if (operation === "=") {
-      screenUpdater(result);
+      equalizer();
       previousNumber = result;
     }
-    // screenUpdater(result);
+    screenUpdater(result);
   }
   ++operationCounter;
   index = 0;
+}
+
+function equalizer() {
+  screenUpdater(result);
+  previousNumber = result;
 }
 
 function clarifier() {
@@ -142,25 +172,39 @@ function clarifier() {
   operationCounter = 0;
   index = 0;
   operationIndex = 0;
-  operationsArray =[];
+  operationsArray = [];
 }
 
 function deleter() {
   let deletedNumber = document.getElementById("screen").textContent;
-  deletedNumber = deletedNumber.slice(0, -1);
-  document.getElementById("screen").textContent = "";
-  document.getElementById("screen").textContent += deletedNumber;
-  if(deletedNumber == "") document.getElementById("screen").textContent = "0";
+  if (deletedNumber !== "Infinity") {
+    deletedNumber = deletedNumber.slice(0, -1);
+    document.getElementById("screen").textContent = "";
+    document.getElementById("screen").textContent += deletedNumber;
+    if (deletedNumber == "")
+      document.getElementById("screen").textContent = "0";
+  } else {
+    (currentNumber = 0),
+      (previousNumber = 0),
+      (document.getElementById("screen").textContent = "0");
+  }
 }
 
 function dotter() {
-  if(document.getElementById("screen").textContent.includes(".") !== true){
+  if (document.getElementById("screen").textContent.includes(".") !== true) {
     document.getElementById("screen").textContent += ".";
   }
   index++;
 }
 
 function screenUpdater(result) {
-  result = Math.round(result*10000)/10000;
+  result = Math.round(result * 10000) / 10000;
   document.getElementById("screen").textContent = result;
+  if (result >= 99999999999) {
+    // for (let i = 0; i < result.toString().length; i++) {
+    //   newString = result.toString().slice(0, 10 - result.toString().length);
+    // }
+    document.getElementById("screen").textContent = "Infinity";
+    console.log(newString);
+  }
 }
